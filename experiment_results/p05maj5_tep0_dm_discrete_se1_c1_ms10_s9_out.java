98ca57ed6b1e7c1f7f5dabfff19dd10cae449da8
PSMFg <alexandre.correia@ifsertao-pe.edu.br>
2019-05-17 21:52:27 -0300

    p05maj5

---
+++ experiment_results/p05maj5_tep0_dm_discrete_se1_c1_ms10_s9_out.java
@@ -0,0 +1,27 @@
+public class p05maj5_dm_discrete_se1_c1_ms10_s9_out {
+	public static void main(String[] args) {
+		
+		Integer InLoc3 = Integer.parseInt(args[3]);
+		Integer InLoc2 = Integer.parseInt(args[2]);
+		Integer InLoc1 = Integer.parseInt(args[1]);
+		Integer InLoc4 = Integer.parseInt(args[4]);
+		Integer InLoc5 = Integer.parseInt(args[5]);
+		Integer InLoc8 = Integer.parseInt(args[8]);
+		Integer OutLoc = 0;
+		Integer InLoc6 = Integer.parseInt(args[6]);
+		Integer InLoc7 = Integer.parseInt(args[7]);
+		
+		OutLoc = InLoc4 + InLoc5 + InLoc6 + InLoc7 + InLoc8;
+		if(OutLoc > InLoc3) {
+			OutLoc = InLoc2;
+		} else {
+			OutLoc = InLoc1;
+		}
+		
+		if(OutLoc==Integer.parseInt(args[0])) {
+			System.out.println("Passed :) Expected: "+args[0]+". Returned: "+ OutLoc +". ");
+		} else {
+			System.out.println("Failed :( Expected: "+args[0]+". Returned: "+ OutLoc +". ");
+		}
+	}
+}
